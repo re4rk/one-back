c3235c63477e52e61cd4e697778ff5631f09b37a
re4rk <37167652+re4rk@users.noreply.github.com>
2023-08-22 20:04:16 +0900

    init project

---
+++ src/main/kotlin/re4rk/oneback/OneBackApplication.kt
@@ -0,0 +1,11 @@
+package re4rk.oneback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OneBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<OneBackApplication>(*args)
+}
---
+++ src/test/kotlin/re4rk/oneback/OneBackApplicationTests.kt
@@ -0,0 +1,12 @@
+package re4rk.oneback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OneBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
